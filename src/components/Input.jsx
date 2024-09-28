9816963b477a9e0ae116dafcad91adb48796a5a0
Guilherme Souza <guilhermemoraes.dev@gmail.com>
2024-09-28 14:38:41 -0300

    title and button components

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -8,3 +8,4 @@ function Input(props) {
 }
 
 export default Input;
+    
--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -1,5 +1,6 @@
 import { ChevronLeftIcon } from "lucide-react";
 import { useNavigate, useSearchParams } from "react-router-dom";
+import Title from "../components/Title";
 
 function TaskPage() {
   const [searchParams] = useSearchParams();
@@ -9,7 +10,7 @@ function TaskPage() {
   const navigate = useNavigate();
 
   return (
-    <div className="h-screen w-screen bg-slate-500 p6 flex justify-center py-4">
+    <div className="h-screen w-screen bg-slate-500 p6">
       <div className="w-[500px] space-y-4">
         <div className="flex justify-center relative mb-6">
           <button
@@ -18,15 +19,11 @@ function TaskPage() {
           >
             <ChevronLeftIcon />
           </button>
-          <h1 className="text-3xl text-slate-100 font-bold text-center">
-            Detalhes da tarefa
-          </h1>
+          <Title>Detalhes da tarefa</Title>
         </div>
 
         <div className="bg-slate-200 p-4 rounded-md">
-          <h2 className="text-xl text-white font-bold text-slate-600">
-            {title}
-          </h2>
+          <h2 className="text-xl font-bold text-slate-600">{title}</h2>
           <p className="text-slate-600">{description}</p>
         </div>
       </div>
